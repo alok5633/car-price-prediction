40f14ea0fda1190bb5dd2a0b4dc8991fe844d7b6
Alok <alok5633y@gmail.com>
2020-03-23 17:32:50 +0530

    reading csv file

---
+++ pred.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 23 17:13:53 2020
+
+@author: Alok
+"""
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+
+sns.set(rc={'figure.figsize':(11.7,8.27)})
+
+cars_data=pd.read_csv('cars_sampled.csv')
+#print(cars_data)
